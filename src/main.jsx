ee68b035b08dc56cd6262dffe4694e8244fd797e
mendsalbert <mendsalbert@gmail.com>
2024-06-30 12:22:52 +0000

    refactor: streamline main.jsx imports
    Removes redundant line space in main.jsx improving import readability and consistency. Adheres to best practices for code formatting and readability, enhancing overall code aesthetics.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import { StateContextProvider } from "./context";
 import App from "./App";
 import "./index.css";
