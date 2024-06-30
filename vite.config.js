ac45ea38e3052585411f3342a05135244ccad587
mendsalbert <mendsalbert@gmail.com>
2024-06-30 13:58:07 +0000

    build(deps): update package.json, vite.config.js and yarn.lock

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      jsx: "automatic",
+    },
+  },
   base: "./",
 });
